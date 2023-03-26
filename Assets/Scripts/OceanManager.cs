8c710fa588f397297e2aef4c6c5335cb90e83ccf
Dinis Dinislamov <forwator221@gmail.com>
2023-03-26 09:26:07 +0500

    Initial commit

---
+++ Assets/Scripts/OceanManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanManager : MonoBehaviour
+{
+
+    public float waveHeight =1f;
+
+    public float waveScale = 1f;
+
+    public float waveSpeed = 1f;
+
+    public Transform ocean;
+
+    Material oceanMaterial;
+
+    Texture2D displacementWaves;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetVariables();
+    }
+
+    void  SetVariables()
+    {
+        oceanMaterial = ocean.GetComponent<Renderer>().sharedMaterial;
+        displacementWaves = (Texture2D)oceanMaterial.GetTexture("_WavesDisplacement");
+    }
+
+    public float WaterHeightAtPosition(Vector3 position)
+    {
+        return ocean.position.y + displacementWaves.GetPixelBilinear(position.x * waveScale /100, position.z * waveScale/100 + Time.time * waveSpeed/100).g * waveHeight/100 * ocean.localScale.x;
+    }
+
+    private void OnValidate()
+    {
+        if(!oceanMaterial)
+        {
+            SetVariables();
+        }
+        UpdateMaterial();
+    }
+    void UpdateMaterial()
+    {
+        oceanMaterial.SetFloat("_WaveScale", waveScale/100);
+        oceanMaterial.SetFloat("_WaveSpeed", waveSpeed/100);
+        oceanMaterial.SetFloat("_WaveHeight", waveHeight/100);
+    }
+}
